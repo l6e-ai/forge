4a41494d5f95e479c038ea9c74f6e1d81c8a4cb2
Bennett Dixon <bennettdixon16@gmail.com>
2025-08-21 16:59:16 -0400

    fix: remove site from public repo

--- site/l6e/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Nav } from "./Nav";
-export { default as Hero } from "./Hero";
-export { default as Features } from "./Features";
-export { default as Integrations } from "./Integrations";
-export { default as GettingStarted } from "./GettingStarted";
-export { default as Footer } from "./Footer";
-export { default as Section } from "./Section";
-export { Button } from "./Button";
-
