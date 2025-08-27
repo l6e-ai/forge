15e92da7111dc3d25c80a527630b3db9b907053d
Bennett Dixon <bennettdixon16@gmail.com>
2025-08-27 16:50:15 -0400

    fix: docs config for site urls

--- site/docs/docusaurus.config.ts
+++ site/docs/docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://l6e-ai.github.io',
+  url: 'https://docs.l6e.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/forge/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
